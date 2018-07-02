7c430b7f3129563793c6b94e7abcc4b92b2b35cf
marc0601 <marckhalil06@gmail.com>
2018-07-02 21:12:54 +0200

    replace "reboot bootloader" with "reboot download"

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,6 +34,8 @@ TW_EXCLUDE_SUPERSU := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_MAX_BRIGHTNESS := 255
+TW_HAS_DOWNLOAD_MODE := true
+TW_NO_REBOOT_BOOTLOADER := true
 include $(LOCAL_PATH)/kernel.mk
 include device/generic/twrpbuilder/BoardConfig32.mk
 
